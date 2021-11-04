b9844b9cbdd265a1966ff53e92142e1dbabede63
EnzoLivelli2000 <enzo.livelli@gmail.com>
2021-11-03 20:38:34 -0500

    starting with devlopment apiOpenWorld

---
+++ src/main/java/com/acme/openworldapi/OpenWorldApiApplication.java
@@ -0,0 +1,13 @@
+package com.acme.openworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWorldApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenWorldApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/acme/openworldapi/shared/domain/model/AuditModel.java
@@ -0,0 +1,27 @@
+package com.acme.openworldapi.shared.domain.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_at", "updated_at"}, allowGetters = true)
+public class AuditModel {
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column (name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column (name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+}
+
