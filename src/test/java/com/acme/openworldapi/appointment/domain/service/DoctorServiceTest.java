6f96762d013c2152bb48919d5c1922329f337422
acostalinan <brunosanto123@hotmail.com>
2021-11-20 13:47:23 -0500

    New Update
    Update with Tests

---
+++ src/test/java/com/acme/openworldapi/appointment/domain/service/DoctorServiceTest.java
@@ -0,0 +1,28 @@
+package com.acme.openworldapi.appointment.domain.service;
+
+import com.acme.openworldapi.appointment.domain.model.entity.Doctor;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoctorServiceTest {
+
+    @Test
+    void getAll() {
+
+    }
+
+    @Test
+    void testGetAll() {
+
+    }
+
+    @Test
+    void create() {
+
+    }
+}
---
+++ src/test/java/com/acme/openworldapi/appointment/domain/service/PatientServiceTest.java
@@ -0,0 +1,28 @@
+package com.acme.openworldapi.appointment.domain.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientServiceTest {
+
+    @Test
+    void getAll() {
+
+    }
+
+    @Test
+    void testGetAll() {
+
+    }
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void getById() {
+
+    }
+}
