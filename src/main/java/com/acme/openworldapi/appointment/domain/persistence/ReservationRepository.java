548cf3d100080ed4ac399f7cc16437beb46f6402
EnzoLivelli2000 <enzo.livelli@gmail.com>
2021-11-25 03:02:26 -0500

    deploy

--- src/main/java/com/acme/openworldapi/appointment/domain/persistence/ReservationRepository.java
+++ src/main/java/com/acme/openworldapi/appointment/domain/persistence/ReservationRepository.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    @Query("select l from Reservation l where l.patient.id = 1 and l.doctor.id = 1")
     List<Reservation> findByDoctorIdAndPatientId();
 
     List<Reservation> findByPatientId(Long ReservationId);
